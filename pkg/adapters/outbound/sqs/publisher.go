9ae665e2188340fb71c74a6eb2221ec93ca9f36a
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-09-06 01:38:14 -0300

    refactory

--- pkg/adapters/outbound/sqs/publisher.go
+++ pkg/adapters/outbound/sqs/publisher.go
@@ -13,7 +13,7 @@ import (
 
 type (
 	AssyncPublisher interface {
-		Publish(ctx context.Context, req *assync.QueueRequest, queueUrl string) (*assync.QueueTriggerResponse, error)
+		Publish(ctx context.Context, req assync.QueueRequest, queueUrl string) (*assync.QueueTriggerResponse, error)
 	}
 
 	assyncPublisher struct {
@@ -30,8 +30,7 @@ func NewAssyncPublisher() AssyncPublisher {
 		client: sqs.NewFromConfig(cfg),
 	}
 }
-
-func (a assyncPublisher) Publish(ctx context.Context, req *assync.QueueRequest, queueUrl string) (*assync.QueueTriggerResponse, error) {
+func (a assyncPublisher) Publish(ctx context.Context, req assync.QueueRequest, queueUrl string) (*assync.QueueTriggerResponse, error) {
 	queueURL := queueUrl
 	content, err := json.Marshal(req)
 	if err != nil {
