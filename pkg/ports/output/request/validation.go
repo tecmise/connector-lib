bd709db465f800c8132a4e2dad8e1b6806e494e1
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-09-10 00:24:57 -0300

    valida object

--- pkg/ports/output/request/validation.go
+++ pkg/ports/output/request/validation.go
@@ -1,6 +1,9 @@
 package request
 
-import "github.com/go-playground/validator/v10"
+import (
+	"errors"
+	"github.com/go-playground/validator/v10"
+)
 
 type (
 	Validatable interface {
@@ -12,3 +15,13 @@ type (
 		Method func(fl validator.FieldLevel) bool
 	}
 )
+
+func ValidateObject(req Validatable, validations ...CustomValidator) error {
+	if req == nil {
+		return errors.New("the request cannot be nil")
+	}
+	if err := req.Validate(validations...); err != nil {
+		return err
+	}
+	return nil
+}
